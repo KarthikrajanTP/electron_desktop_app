557260e1d912394725cad084fb89f48283a93684
Karthikrajan Vijayakumar <karthikrajan@testpress.in>
2025-11-17 20:54:27 +0530

    fix: Add device details to local storage

--- main.js
+++ main.js
@@ -1,12 +1,67 @@
-const { app, components,BrowserWindow } = require('electron');
+const { app, components, BrowserWindow } = require('electron');
+const os = require('os');
+const crypto = require('crypto');
+
+function getMachineInfo() {
+  const networkInterfaces = os.networkInterfaces();
+  const allMacs = [];
+  let primaryMacAddress = '';
+
+  // Collect all MAC addresses
+  for (const interfaceName in networkInterfaces) {
+    const interfaces = networkInterfaces[interfaceName];
+    for (const iface of interfaces) {
+      if (iface.mac && iface.mac !== '00:00:00:00:00:00' && !iface.internal) {
+        allMacs.push(iface.mac);
+        if (!primaryMacAddress) {
+          primaryMacAddress = iface.mac;
+        }
+      }
+    }
+  }
+
+  // Generate machineId from MAC address and hostname
+  const machineIdSource = `${primaryMacAddress}-${os.hostname()}`;
+  const machineId = crypto.createHash('sha256').update(machineIdSource).digest('hex');
+
+  return {
+    machineName: os.hostname(),
+    macAddress: primaryMacAddress || '00:00:00:00:00:00',
+    machineId: machineId,
+    allMacs: allMacs.join(','),
+    os: os.platform(),
+    flag: process.platform === 'win32' ? 'win' : process.platform === 'darwin' ? 'mac' : 'linux',
+    appVersion: '8081'
+  };
+}
+
+function injectLocalStorage(webContents, machineInfo) {
+  const script = `
+    localStorage.setItem('machineName', '${machineInfo.machineName}');
+    localStorage.setItem('macAddress', '${machineInfo.macAddress}');
+    localStorage.setItem('machineId', '${machineInfo.machineId}');
+    localStorage.setItem('allMacs', '${machineInfo.allMacs}');
+    localStorage.setItem('os', '${machineInfo.os}');
+    localStorage.setItem('flag', '${machineInfo.flag}');
+    localStorage.setItem('appVersion', '${machineInfo.appVersion}');
+  `;
+  webContents.executeJavaScript(script);
+}
 
 function createWindow() {
+  const machineInfo = getMachineInfo();
+  
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
     }
+  });
+
+  // Inject localStorage before page loads
+  win.webContents.on('did-finish-load', () => {
+    injectLocalStorage(win.webContents, machineInfo);
   });
 
   win.loadURL('https://app.theuniqueacademycommerce.com/dashboard');
