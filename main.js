6a1ac3c6b96bbdaa40c73476edd0bd5f80fed3b1
Karthikrajan Vijayakumar <karthikrajan@testpress.in>
2025-11-17 18:22:52 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, components,BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('https://app.theuniqueacademycommerce.com/dashboard');
+}
+app.whenReady().then(async () => {
+  await components.whenReady();
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
